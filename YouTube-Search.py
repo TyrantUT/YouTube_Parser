5b2c3a212e588c3f2267dd54099ee65e149ba803
TyrantUT <16949372+TyrantUT@users.noreply.github.com>
2023-01-16 14:22:24 -0800

    Update YouTube-Search.py

--- YouTube-Search.py
+++ YouTube-Search.py
@@ -11,7 +11,7 @@ import time
 import os
 import glob
 
-DEVELOPER_KEY = "AIzaSyAOeOazXFgWKfMIcSZ2Crhgclr6VIy0MPI"
+DEVELOPER_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
